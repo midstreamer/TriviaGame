4ccdf38c6345281066d14a725319e7e175c58828
midstreamer <chadalessi@yahoo.com>
2018-03-02 13:24:26 -0800

    added formatting to div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 var seconds;
     var temp;
-
+$("#correct").text(correctAnswers); //this returns the text wins for the element win in the html doc
+$("#incorrect").text(incorrectAnswers); //this returns the text losses for the element loss in the html doc
+    
     function countdown() {
         seconds = document.getElementById("countdown").innerHTML;
         seconds = parseInt(seconds, 10);
@@ -19,3 +23,25 @@ var seconds;
     }
 
     countdown();
+
+
+    //$(".btn-sm").on("click", function(){
+        //console.log("working");
+    $(".btn-sm").on("click", function(){
+        if(document.getElementById("correctAnswer").checked) {
+            correctAnswers++;
+            $("#correct").text(correctAnswers);
+        }
+
+          
+    });
+        
+        
+    
+        //$("#yourScore").text(counter);
+        //if (counter == Random){
+           // won();
+       // }
+       // else if (counter > Random){
+            //lost();
+        //}
